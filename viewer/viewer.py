b90d8b226f84be9c526c783a1b7344ac01ef6713
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-06-25 18:41:51 +0900

    need to change to multithreading?

--- viewer/viewer.py
+++ viewer/viewer.py
@@ -1,13 +1,15 @@
 import pygame
 import numpy as np
 from queue import Queue
+from threading import Thread
 
-class Viewer() :
+class Viewer(Thread) :
     """
     This module shows a numpy array(3D) on a display
+    String 'Terminate' will terminate the viewer
     """
     def __init__(self, width:int, height:int, event_queue:Queue,
-                 image_queue:Queue):
+                 image_queue:Queue, etc_queue:Queue, fps=60):
         """
         Initialize Viewer
 
@@ -16,33 +18,64 @@ class Viewer() :
         height : Height of the screen (Default 720)
         event_queue: a Queue to put events that happended in Viewer
         image_queue: a Queue to get image array
+        etc_queue: a Queue to get any info like 'terminate'
         """
+        super().__init__(daemon=False)
         pygame.init()
-        self._width = width
-        self._height = height
-        self._screen = pygame.display.set_mode((self._width, self._height),
-                                                pygame.RESIZABLE)
+        self.size = (width, height)
+        self._screen = pygame.display.set_mode(self.size, pygame.RESIZABLE)
         self._clock = pygame.time.Clock()
         self._event_queue = event_queue
         self._image_queue = image_queue
+        self._etc_queue = etc_queue
+        self._fps = fps
 
-    def run(self, initial_image:np.array) :
+    def run(self) :
         """
         Run viewer's mainloop
         """
         mainloop = True
-        if initial_image.shape != (self._height, self._width):
-            
-        if direct :
-            pygame.surfarray.blit_array(self._screen, surfarray)
-        else :
-            surface = pygame.surfarray.make_surface(surfarray)
-            self._screen.blit(surface, (0,0))
-        pygame.display.flip()
+        while mainloop :
+            self._clock.tick(self._fps)
+            if not self._image_queue.empty():
+                image = self._image_queue.get()
+                if image.shape[0:2] != self.size:
+                    self.size = image.shape[0:2]
+                    self._screen = pygame.display.set_mode(self.size)
+                pygame.surfarray.blit_array(self._screen, image)
+            if not self._etc_queue.empty():
+                external_event = self._etc_queue.get()
+                if external_event == 'Terminate' :
+                    mainloop = False
+            ###escape
+            for event in pygame.event.get() :
+                if event.type == pygame.QUIT :
+                    mainloop = False
+                elif event.type == pygame.KEYDOWN :
+                    if event.key == pygame.K_ESCAPE :
+                        mainloop = False 
+                        return
+            ######################################          
+        # if direct :
+        #     pygame.surfarray.blit_array(self._screen, surfarray)
+        # else :
+        #     surface = pygame.surfarray.make_surface(surfarray)
+        #     self._screen.blit(surface, (0,0))
+            pygame.display.flip()
 
     @property
     def size(self):
-        return(self._width, self._height)
+        """
+        size : (width, height)
+        """
+        return (self._width, self._height)
+
+    @size.setter
+    def size(self, size:tuple):
+        """
+        size : (width, height)
+        """
+        self._width, self._height = size
 
     def close(self):
         pygame.quit()
@@ -50,4 +83,10 @@ class Viewer() :
 #testing
 if __name__ == '__main__':
     import time
-    a = np.random.randint(0,255,(720,720,3),dtype=np.uint8)
+    imgQ = Queue()
+    evntQ = Queue()
+    etcQ = Queue()
+    v = Viewer(720, 300, evntQ, imgQ, etcQ)
+    v.start()
+    newimg = np.ones((600,600,3), dtype=np.uint8)*100
+    imgQ.put(newimg)
